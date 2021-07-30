8a4c93d43b7508dafdcf50f7b981dac11014ca51
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-07-30 15:39:27 +0530

    feat: added server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app  = express();
+
+//middleware has access to req and res
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+//define a simple route
+app.get('/',(req,res) => {
+    res.json({"message":"Welcome to the Bookstore application"});
+});
+
+//listen for request
+app.listen(3000,()=>{
+    console.log(`Server is listening at 3000`);
+})
